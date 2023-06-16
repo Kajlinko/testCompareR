a418670b29aa3d40e73f4e7c68777f09acea3d17
Kajlinko <kyle.wilson@liverpool.ac.uk>
2023-06-16 19:43:47 +0200

    Preparing for CRAN submission

--- tests/testthat/test_interpretR.R
+++ tests/testthat/test_interpretR.R
@@ -3,11 +3,10 @@
 #-------------------------------------------------------------------------------
 
 test_that("interpretR", {
-
   # Test 1: Valid argument
 
-  test1 <- c(rep(1,300), rep(0,100), rep(1, 55), rep(0,145))
-  test2 <- c(rep(1,280), rep(0,120), rep(1, 45), rep(0,155))
+  test1 <- c(rep(1, 300), rep(0, 100), rep(1, 55), rep(0, 145))
+  test2 <- c(rep(1, 280), rep(0, 120), rep(1, 45), rep(0, 155))
   gold <- c(rep(1, 400), rep(0, 200))
 
   df <- data.frame(test1, test2, gold)
@@ -24,8 +23,8 @@ test_that("interpretR", {
 
   # Test 3: Tests have exactly equal, very poor performance
 
-  test1 <- c(rep(1,10), rep(0,390), rep(1, 190), rep(0,10))
-  test2 <- c(rep(1,10), rep(0,390), rep(1, 190), rep(0,10))
+  test1 <- c(rep(1, 10), rep(0, 390), rep(1, 190), rep(0, 10))
+  test2 <- c(rep(1, 10), rep(0, 390), rep(1, 190), rep(0, 10))
   gold <- c(rep(1, 400), rep(0, 200))
 
   df <- data.frame(test1, test2, gold)
@@ -33,5 +32,4 @@ test_that("interpretR", {
   result <- compareR(df)
 
   expect_no_error(interpretR(result))
-
-  })
+})
